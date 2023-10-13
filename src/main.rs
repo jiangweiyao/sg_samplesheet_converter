a8f7896acbcf83346434cca1448ef937401fe634
Jiangwei Yao <yao.jiangwei@gmail.com>
2023-10-13 16:57:29 -0400

    addition and refactor pt2

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,14 @@ use std::collections::HashMap;
 type Record = HashMap<String, String>;
 
 const SAMPLE_ID: &str = "Sample_ID";
-const INDEX1_SEQUENCE: &str = "INDEX1_SEQUENCE";
-const INDEX2_SEQUENCE: &str = "INDEX2_SEQUENCE";
+const INDEX1_SEQUENCE: &str = "Index1_Sequence";
+const INDEX2_SEQUENCE: &str = "Index2_Sequence";
 
 fn converter(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut output_name_rc = String::from("_rc.csv");
-    output_name_rc.insert_str(0, output);
-    
-    let mut output_name_fs = String::from("_fs.csv");
-    output_name_fs.insert_str(0, output);
-    //let mut output_name_fs = output.clone();
 
-    let mut rdr = csv::Reader::from_path(input)?;
-    let mut wtr_rc = csv::Writer::from_path(output_name_rc)?;
-    wtr_rc.write_record(&["Sample_ID", "INDEX1_SEQUENCE", "INDEX2_SEQUENCE"]);
-    let mut wtr_fs = csv::Writer::from_path(output_name_fs)?;
-    wtr_fs.write_record(&["Sample_ID", "INDEX1_SEQUENCE", "INDEX2_SEQUENCE"]);
+    let mut rdr = csv::Reader::from_path(config.input_file)?;
+    let mut wtr = csv::Writer::from_path(config.output_file)?;
+    wtr.write_record(&["Sample_ID", "INDEX1_SEQUENCE", "INDEX2_SEQUENCE"]);
 
     for result in rdr.deserialize() {
         let record: Record = result?;
@@ -41,15 +33,19 @@ fn converter(config: Config) -> Result<(), Box<dyn Error>> {
         //println!("{:?}", record.values());
         //let samplename = record.get(SAMPLE_ID).unwrap();
         let samplename = record.get(SAMPLE_ID).unwrap();
-        //println!("{}", samplename);
+        println!("{}", samplename);
         let index1_seq = record.get(INDEX1_SEQUENCE).unwrap();
         let index2_seq = record.get(INDEX2_SEQUENCE).unwrap();
         let dna_rc1 = dna::revcomp(index1_seq.as_bytes());
         let dna_rc1_string = String::from_utf8_lossy(&*dna_rc1).to_string();
         let dna_rc2 = dna::revcomp(index2_seq.as_bytes());
         let dna_rc2_string = String::from_utf8_lossy(&*dna_rc2).to_string();
-        wtr_rc.write_record(&[&samplename, &dna_rc2_string, &dna_rc1_string]);
-        wtr_fs.write_record(&[&samplename, &index2_seq, &dna_rc1_string]);
+        if config.forstrand {
+            wtr.write_record(&[&samplename, &index2_seq, &dna_rc1_string]);
+        }
+        else {
+            wtr.write_record(&[&samplename, &dna_rc2_string, &dna_rc1_string]);
+        }
         //wtr.write_record(&["City", "State", "Population", "Latitude", "Longitude"])?;
     }
 
@@ -87,8 +83,8 @@ fn main() {
     }
 
     let config = Config { forstrand, input_file, output_file };
-    println!("Config is {} {} {}", config.forstrand, config.input_file, config.output_file);
-
+    //println!("Config is {} {} {}", config.forstrand, config.input_file, config.output_file);
+    converter(config);
 }
 
 pub struct Config {
