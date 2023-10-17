2cb47ae026e7aab0fecc5955756a20c541e85f59
Jiangwei Yao <yao.jiangwei@gmail.com>
2023-10-17 16:59:39 -0400

    v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,13 @@
 use bio::alphabets::dna;
 
-use std::{
-    env,
-    error::Error,
-    ffi::OsString,
-    fs::File,
-    process,
-    io,
-    any::type_name,
-};
-
 use std::collections::HashMap;
+use std::error::Error;
 
 type Record = HashMap<String, String>;
 
 const IL_SAMPLE_ID: &str = "Sample_ID";
-const IL_INDEX1_SEQUENCE: &str = "Index1_Sequence";
-const IL_INDEX2_SEQUENCE: &str = "Index2_Sequence";
+const IL_INDEX1_SEQUENCE: &str = "index";
+const IL_INDEX2_SEQUENCE: &str = "index2";
 
 const SG_SAMPLE_ID: &str = "Sample_ID";
 const SG_INDEX1_SEQUENCE: &str = "Index1_Sequence";
@@ -26,7 +17,7 @@ pub fn converter(config: Config) -> Result<(), Box<dyn Error>> {
 
     let mut rdr = csv::Reader::from_path(config.input_file)?;
     let mut wtr = csv::Writer::from_path(config.output_file)?;
-    wtr.write_record(&[SG_SAMPLE_ID, SG_INDEX1_SEQUENCE, SG_INDEX2_SEQUENCE]);
+    wtr.write_record(&[SG_SAMPLE_ID, SG_INDEX1_SEQUENCE, SG_INDEX2_SEQUENCE])?;
 
     for result in rdr.deserialize() {
         let record: Record = result?;
@@ -81,14 +72,14 @@ pub fn converter(config: Config) -> Result<(), Box<dyn Error>> {
         let dna_rc2_string = String::from_utf8_lossy(&*dna_rc2).to_string();
         */
         if config.forstrand {
-            wtr.write_record(&[&samplename, &index2_seq, &dna_rc1_string]);
+            wtr.write_record(&[&samplename, &index2_seq, &dna_rc1_string])?;
         }
         else {
-            wtr.write_record(&[&samplename, &dna_rc2_string, &dna_rc1_string]);
+            wtr.write_record(&[&samplename, &dna_rc2_string, &dna_rc1_string])?;
         }
     }
 
-
+    wtr.flush()?;
     Ok(())
 }
 
@@ -99,10 +90,4 @@ pub struct Config {
 }
 
 
-fn reverse(s: &str) -> String {
-    s.chars().rev().collect()
-}
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
